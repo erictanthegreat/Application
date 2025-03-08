d7069e6371b7804a3acfac9fc6091d69bfbead5b
erictanthegreat <etan@gbox.adnu.edu.ph>
2025-03-08 15:20:38 +0800

    update on edit profile

--- app/Profile/EditProfile.tsx
+++ app/Profile/EditProfile.tsx
@@ -1,3 +1,10 @@
+/* Authored by: Eric Tan Jr.
+Company: Nvchads
+Project: InvenTori
+Feature: [FEATURECODE-004] Edit Profile
+Description: Let's the user edit and update their profile.
+ */
+
 import React from "react";
 import { Platform } from "react-native";
 import { Text, View } from "../../components/Themed";
@@ -5,9 +12,9 @@ import { StatusBar } from "expo-status-bar";
 
 export default function Home() {
     return (
-        <View>
-            <View className="h-screen flex justify-center items-center gap-[10px]">
-                <Text>Hello world! 👋</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ padding: 20, alignItems: 'center', gap: 10 }}>
+                <Text>Still in progress!</Text>
             </View>
         </View>
     );
