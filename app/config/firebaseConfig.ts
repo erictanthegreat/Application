744229c5cfe8b9b15802a8a2f9ebdc3d6bff9fe0
mmnagrampa <mmnagrampa@gbox.adnu.edu.ph>
2025-05-21 23:24:40 +0800

    Updated Config

--- app/config/firebaseConfig.ts
+++ app/config/firebaseConfig.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCrAGSbTeRZkB2bIJz7tyXPaepUpFtzvVA",
@@ -15,7 +16,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
